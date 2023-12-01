169330d2bc5e32193c4e429e4fe3c31285397246
wbail <guilhermedanbail@gmail.com>
2023-12-01 11:57:58 +0000

    crud

---
+++ Blazor101.Client/BookClient.cs
@@ -0,0 +1,66 @@
+﻿using Blazor101.Client.Models;
+
+namespace Blazor101.Client;
+
+public static class BookClient
+{
+    private static readonly List<Book> books = new()
+    {
+        new Book()
+        {
+            Id = 1,
+            Name = "1984",
+            Genre = "Novel",
+            Price = 9.99M,
+            ReleaseDate = new DateTime(1940, 1, 1)
+        },
+        new Book()
+        {
+            Id = 2,
+            Name = "War of the Worlds",
+            Genre = "Novel",
+            Price = 10.99M,
+            ReleaseDate = new DateTime(1890, 1, 1)
+        },
+        new Book()
+        {
+            Id = 3,
+            Name = "Time Machine",
+            Genre = "Novel",
+            Price = 7.00M,
+            ReleaseDate = new DateTime(1900, 1, 1)
+        }
+    };
+
+    public static Book[] GetBooks()
+    {
+        return books.ToArray();
+    }
+
+    public static void AddBook(Book book)
+    {
+        book.Id = books.Max(book => book.Id) + 1;
+        books.Add(book);
+    }
+
+    public static Book GetBook(int id)
+    {
+        return books.Find(book => book.Id == id) ?? throw new Exception("Could not find the book!");
+    }
+
+    public static void UpdateBook(Book updatedBook)
+    {
+        Book existingBook = GetBook(updatedBook.Id);
+        existingBook.Name = updatedBook.Name;
+        existingBook.Genre = updatedBook.Genre;
+        existingBook.Price = updatedBook.Price;
+        existingBook.ReleaseDate = updatedBook.ReleaseDate;
+    }
+
+    public static void DeleteBook(int id)
+    {
+        Book book = GetBook(id);
+
+        books.Remove(book);
+    }
+}
---
+++ Blazor101.Client/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor101.Client.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    
+    [Required]
+    [StringLength(50)]
+    public required string Name { get; set; }
+    
+    [Required]
+    [StringLength(30)]
+    public required string Genre { get; set; }
+    
+    [Required]
+    [Range(1, 70000)]
+    public decimal Price { get; set; }
+    
+    [Required]
+    public DateTime ReleaseDate { get; set; }
+}
